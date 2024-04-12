d1d61f585a6edda5da46bdf8078761c4b876ead3
MKisken <160752108+MKisken@users.noreply.github.com>
2024-04-12 14:22:38 -0400

    update: imported files

--- index.js
+++ index.js
@@ -1,67 +1,35 @@
-const api_key = '2d980b1e-ae9c-49c2-8ba1-7a34b3d925a7';
-const pps_key_url = 'http://api.ppsapi.com/';
 const api_url = 'http://192.168.1.145:44333/api/';
-const url = "";
 const ping = 20000;
+var numShipped = 0;
+var numOrdered = 0;
 
 const requestOptions = {
-  mode: 'no-cors',
-  headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Origin, Content- Type, X-Auth - Token"
-    }
+    method: 'GET'
 };
 
-//'x-access-token'
-
-window.onload = function() {
-    console.log("This is called!");
-}
-
-/*
 var myInterval = window.setInterval(function(){
     getOrders();
     getShipped();
     getRemainder();
-}, 20000);
-*/
-
-async function test(){
-    try{
-        const response = await fetch(api_url, requestOptions);
-        if(!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        const operator = await response.json;
-        console.log(operator);
-    } catch (error) {
-        console.error("There has been a problem with your fetch operation: ", error);
-    }
-}
-
+}, ping);
 
 //get the order calling the orders api and setting the value
 async function getOrders(){
 
-    const orders = document.getElementById("ordered");
-
-    url = api_url + "mailorder/orders";
+    const orders_count = document.getElementById("ordered");
 
     try{
 
-        const response = await fetch(url, requestOptions);
+        const response = await fetch(api_url + "mailorder/orders", requestOptions);
         if(!response.ok) {
             throw new Error("Network response was not ok");
         }
 
-        const output = await response.json;
+        const output = await response.json();
 
         console.log(output);
-        orders.textContent = output;
-
-        return output;
+        orders_count.textContent = JSON.stringify(output);
+        numOrdered = parseInt(JSON.stringify(output));
 
     } catch (error) {
         console.error("There has been a problem with your fetch operation: ", error);
@@ -71,21 +39,20 @@ async function getOrders(){
 //get the order calling the orders api and setting the value
 async function getShipped(){
 
-    const orders = document.getElementById("shipped");
-
-    url = api_url + "mailorder/orders/shipped";
+    const shipped_count = document.getElementById("shipped");
 
     try{
 
-        const response = await fetch(url, requestOptions);
+        const response = await fetch(api_url + "mailorder/orders/shipped", requestOptions);
         if(!response.ok) {
             throw new Error("Network response was not ok");
         }
 
-        const shipped = await response.json;
-        console.log(shipped);
+        const output = await response.json();
+        console.log(output);
+        shipped_count.textContent = JSON.stringify(output);
 
-        return shipped;
+        numShipped = parseInt(JSON.stringify(output));
 
     } catch (error) {
         console.error("There has been a problem with your fetch operation: ", error);
@@ -95,7 +62,7 @@ async function getShipped(){
 //get the remainder of ordered and shipped
 function getRemainder() {
 
-    const orders = document.getElementById("remaining");
+    const remining_count = document.getElementById("remaining");
 
-    orders.textContent = (getOrders() - getShipped());
+    remining_count.textContent = (numOrdered - numShipped);
 }
